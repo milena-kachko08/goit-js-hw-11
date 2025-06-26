bb3bb71261cd159d410e0ccf5974c300bca46ab3
Milena Kachko <kachkomilena@gmail.com>
2025-06-26 15:59:39 +0200

    add

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
-const loader = document.querySelector('.loader');
 
 let currentPage = 1;
 let currentQuery = '';
